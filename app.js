41df5a6c31e08228c4552b255a85d249ea476a8f
Michelle Salazar <michellesalazar010@gmail.com>
2020-04-04 18:30:49 -0700

    msquestion4 collaborative effort between table1

--- app.js
+++ app.js
@@ -77,14 +77,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
-
+    console.log(testArray);
 function sumArray(sumArr) { //eslint-disable-line
-
+    var tls = 0;
+    for(var i = 0; i < sumArr.length; i++){
+        tls = sum(tls, sumArr[i])[0];
+    }
+    return[tls, sumArr + ' was passed in as an array of numbers, and '+tls+' is their sum.']
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
